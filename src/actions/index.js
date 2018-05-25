270d318451a1732b85c9aa11b09f49d1a9cda215
yla139 <yla139.lin@gmail.com>
2018-05-24 22:35:13 -0700

    add raction creater

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,18 @@
+import axios from 'axios';
+
 const API_KEY = '8c61801365c45fac2e394e6caf86eff9';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`; //es6 syntax
+
+export const FETCH_WEATHER = 'FETCH_WEATHER'
+
+// action creater
+export function fetchWeather(city) {
+    const url = `${ROOT_URL}&q=${city},us`;
+    const request = axios.get(url);
+
+    // return action with type
+    return {
+        type: FETCH_WEATHER,
+        payload: request
+    };
+}
