4cd40a72e181f08509676e0e1135d8d3cc00490a
yla139 <yla139.lin@gmail.com>
2018-05-29 22:48:40 -0700

    redux promise

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { fetchWeather } from '../actions/index';
 
-export default class SearchBar extends Component {
+
+class SearchBar extends Component {
     constructor(props) {
         super(props);
         this.state = { term: '' };
         this.onInputChange = this.onInputChange.bind(this);
+        this.onFormSubmit = this.onFormSubmit.bind(this);
     }
 
     onInputChange(event) {
@@ -13,6 +18,10 @@ export default class SearchBar extends Component {
 
     onFormSubmit(event) {
         event.preventDefault(); // tells the browser don't submit the form
+
+        // fetch weather data
+        this.props.fetchWeather(this.state.term);
+        this.setState({ term: '' });
     }
 
     render() {
@@ -30,3 +39,9 @@ export default class SearchBar extends Component {
         );
     }
 }
+
+function mapDispatchToProps(dispatch) {
+    return bindActionCreators({ fetchWeather }, dispatch);
+}
+
+export default connect(null, mapDispatchToProps)(SearchBar);
