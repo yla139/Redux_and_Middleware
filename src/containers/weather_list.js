12002e4287160428e0542a13e35dec3710215353
yla139 <yla139.lin@gmail.com>
2018-06-24 22:20:27 -0700

    fix chart

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -11,14 +11,13 @@ class WeatherList extends Component {
         const pressures = cityData.list.map(weather => weather.main.pressure);
         const humidities = cityData.list.map(weather => weather.main.humidity);
 
+        console.log(pressures);
         return (
             <tr key={name}>
                 <td>{name}</td>
-                <td>
-                    <Chart data={temps} color="orange" />
-                    <Chart data={pressures} color="red" />
-                    <Chart data={humidities} color="green" />
-                </td>
+                <td><Chart data={temps} color="orange" units="K" /></td>
+                <td><Chart data={pressures} color="green" units="hPa" /></td>
+                <td><Chart data={humidities} color="black" units="%" /></td>
             </tr>
         );
     }
@@ -29,9 +28,9 @@ class WeatherList extends Component {
                 <thead>
                     <tr>
                         <th>City</th>
-                        <th>Temperature</th>
-                        <th>Pressure</th>
-                        <th>Humidity</th>
+                        <th>Temperature (K)</th>
+                        <th>Pressure (hPa)</th>
+                        <th>Humidity (%)</th>
                     </tr>
                 </thead>
                 <tbody>
